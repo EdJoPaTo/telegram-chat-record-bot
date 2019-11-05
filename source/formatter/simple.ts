ea34a8af3355dd7dcaefdfd8cd0b879769cd5a84
Edgar Toll <edjopato@gmail.com>
2019-11-05 13:09:25 +0100

    feat(raw): add newline at the end

--- source/formatter/simple.ts
+++ source/formatter/simple.ts
@@ -2,6 +2,6 @@ import {Message} from 'telegram-typings'
 import {Result} from './type'
 
 export function raw(history: readonly Message[]): Result[] {
-	const content = JSON.stringify(history, undefined, '\t')
+	const content = JSON.stringify(history, undefined, '\t') + '\n'
 	return [{content, filenameSuffix: 'raw.json'}]
 }

5f362dd062d616aa1e7db796320b5d76d8920323
Edgar Toll <edjopato@gmail.com>
2019-11-05 13:15:34 +0100

    feat(plaintext): add plaintext formatter

--- source/formatter/index.ts
+++ source/formatter/index.ts
@@ -2,18 +2,21 @@ import {Message} from 'telegram-typings'
 
 import {Result} from './type'
 
+import {plaintext} from './plaintext'
 import {raw} from './simple'
 
 export * from './type'
 export * from './simple'
 
-export type FormatType = 'raw'
-export const FORMATS: FormatType[] = ['raw']
+export type FormatType = 'raw' | 'plaintext'
+export const FORMATS: FormatType[] = ['raw', 'plaintext']
 
 export function formatByType(history: readonly Message[], type: FormatType): Result[] {
 	switch (type) {
 		case 'raw':
 			return raw(history)
+		case 'plaintext':
+			return plaintext(history)
 		default:
 			throw new Error('unknown type')
 	}
---
+++ source/formatter/plaintext.ts
@@ -0,0 +1,94 @@
+import {Message, User} from 'telegram-typings'
+import {Result} from './type'
+
+export function plaintext(history: readonly Message[]): Result[] {
+	const messageDict = history
+		.reduceRight((coll: Record<number, Message>, add) => {
+			const msgId = add.message_id
+			if (coll[msgId] === undefined) {
+				coll[msgId] = add
+			}
+
+			return coll
+		}, {})
+
+	const messages = Object.values(messageDict)
+	const entries = messages
+		.map(o => formatIndividualMessage(o))
+		.filter(o => o)
+	const linuxContent = entries.join('\n') + '\n'
+	const windowsContent = linuxContent.replace(/\n/g, '\r\n')
+
+	return [{content: windowsContent, filenameSuffix: 'plaintext.txt'}]
+}
+
+function formatIndividualMessage(message: Message): string {
+	const sender = formatUser(message.from)
+	const timestamp = formatTimestamp(message.date)
+	const text = formatContent(message)
+
+	return `${timestamp} <${sender}> ${text}`
+}
+
+function formatUser(user: User | undefined): string {
+	if (!user) {
+		return ''
+	}
+
+	let sender = user.first_name
+	if (user.last_name) {
+		sender += ' '
+		sender += user.last_name
+	}
+
+	return sender
+}
+
+function formatTimestamp(unixTimestamp: number): string {
+	return new Date(unixTimestamp * 1000).toISOString().slice(0, -5)
+}
+
+const OTHER_MESSAGE_TYPE_EXCLUDE: Array<keyof Message> = [
+	'caption',
+	'chat',
+	'date',
+	'edit_date',
+	'from',
+	'message_id',
+	'reply_to_message',
+	'text'
+]
+
+function formatContent(message: Message): string {
+	const parts: string[] = []
+
+	if (message.forward_from) {
+		const from = formatUser(message.forward_from)
+		parts.push(`FORWARDED FROM <${from}>`)
+	}
+
+	if (message.reply_to_message) {
+		const repliedToName = formatUser(message.reply_to_message.from)
+		const repliedToContent = formatContent(message.reply_to_message)
+		const shorted = repliedToContent.length > 20 ? repliedToContent.slice(0, 20) + '…' : repliedToContent
+
+		parts.push(`REPLY TO <${repliedToName}> "${shorted}"`)
+	}
+
+	const contentTypes = (Object.keys(message) as Array<keyof Message>)
+		.filter(o => !OTHER_MESSAGE_TYPE_EXCLUDE.includes(o))
+
+	if (contentTypes.length > 0) {
+		parts.push(`<${contentTypes.join(',')}>`)
+	}
+
+	if (message.text) {
+		parts.push(message.text)
+	}
+
+	if (message.caption) {
+		parts.push(message.caption)
+	}
+
+	return parts.join(' ')
+}

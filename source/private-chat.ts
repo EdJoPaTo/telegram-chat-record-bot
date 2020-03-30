41d26200ef0ca65ab96c2791601a35408b55a6e5
Edgar Toll <edjopato@gmail.com>
2020-03-30 13:17:56 +0200

    refactor(i18n): keep variables out of translation

--- source/private-chat.ts
+++ source/private-chat.ts
@@ -4,7 +4,9 @@ export const bot = new Composer()
 
 bot.command(['start', 'help', 'peek', 'finish'], async ctx => {
 	let text = ''
-	text += (ctx as any).i18n.t('private.start')
-	text += 'https://github.com/EdJoPaTo/telegram-chat-record-bot'
+	text += (ctx as any).i18n.t('private.start', {
+		username: '@EdJoPaTo',
+		repolink: 'https://github.com/EdJoPaTo/telegram-chat-record-bot'
+	})
 	return ctx.reply(text)
 })

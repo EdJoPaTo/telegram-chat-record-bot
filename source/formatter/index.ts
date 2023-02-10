30f3ac44a1343ec1816cfa377f30b4c3a3c9e11f
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-02-10 11:40:00 +0100

    refactor: dont specify type list twice

--- source/formatter/index.ts
+++ source/formatter/index.ts
@@ -6,8 +6,8 @@ import type {Result} from './type.js'
 export * from './type.js'
 export * from './simple.js'
 
-export type FormatType = 'raw' | 'plaintext'
-export const FORMATS: FormatType[] = ['raw', 'plaintext']
+export const FORMATS = ['raw', 'plaintext'] as const
+export type FormatType = typeof FORMATS[number]
 
 export function formatByType(
 	history: readonly Message[],
